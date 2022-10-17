6a6cf2b4c88be5bf74dbd01f374fa83837168e8a
Aphenphos <MaxwillWinters@gmail.com>
2022-10-17 19:07:47 -0400

    can add and remove people to your calendar group

--- src/services/owners.js
+++ src/services/owners.js
@@ -10,7 +10,27 @@ export async function getCalendars() {
   return checkError(response);
 }
 
+export async function getCalendar(name, user1) {
+  const response = await client
+    .from('owners')
+    .select('*')
+    .eq('name', name)
+    .eq('user1', user1)
+    .single();
+  console.log(response.data);
+  return response.data;
+}
 export async function updateCalendar(calendar) {
   const response = await client.from('owners').upsert(calendar).single();
   return response.data;
+}
+
+export async function getUserByUserName(username) {
+  const response = await client
+    .from('user-profiles')
+    .select('*')
+    .eq('profile_name', username)
+    .single();
+  if (username === null) return response.data;
+  return response.data.id;
 }
