eb869f69189a1dbd920c196e169d9344008ff6f3
Aphenphos <MaxwillWinters@gmail.com>
2022-10-19 12:53:23 -0400

    even more eslint

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { signOut } from '../../../services/auth';
 import { UserContext } from '../../../context/useUser';
 import { Link } from 'react-router-dom';
