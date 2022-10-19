a03f2d0b98026cc9b206ae6783140898648975bd
Aphenphos <MaxwillWinters@gmail.com>
2022-10-19 19:17:06 -0400

    can remove events and updated accordingly

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,17 +1,38 @@
+import { useState } from 'react';
+import { deleteEvent } from '../../services/events';
 import './Day.sass';
 
 export default function Day({ day, desc }) {
+  const [active, setActive] = useState(null);
   if (day === ' ') {
     return <button className="hidden-button"></button>;
   }
+
+  const removeEvent = async (id) => {
+    setActive(id);
+    await deleteEvent(id);
+  };
   return (
-    <div>
-      <button className="day-button">
+    <>
+      <div>
         {day}
         {desc.map((event, index) => (
-          <p key={index}>{event.description}</p>
+          <>
+            <div key={index} className={active ? 'hidden' : ''}>
+              <p>{event.description}</p>
+              <button
+                id={index}
+                value={event.id}
+                onClick={(e) => {
+                  removeEvent(e.target.value);
+                }}
+              >
+                -
+              </button>
+            </div>
+          </>
         ))}
-      </button>
-    </div>
+      </div>
+    </>
   );
 }
--- src/services/events.js
+++ src/services/events.js
@@ -9,3 +9,8 @@ export async function getEvents(calId) {
   const response = await client.from('calendar-events').select('*').eq('calendar', calId);
   return response.data;
 }
+
+export async function deleteEvent(id) {
+  const response = await client.from('calendar-events').delete().eq('id', id).single();
+  return response.data;
+}
